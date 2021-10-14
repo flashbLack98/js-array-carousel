bb41c078b1d37c0d5abace72d66c269faff400fd
flashbLack98 <npalakuttige98@icloud.com>
2021-10-15 01:20:56 +0200

    button cicle complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let containerHero = document.querySelector(".container_hero");
-
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -8,13 +6,98 @@ const items = [
     'img/05.jpg'
 ];
 
-for (let i = 0; i < 5; i++) {
-    const imgCurrent = items[i];
+let containerHero = document.querySelector(".container_hero");
 
-    let tagImg = `<img  src= "${imgCurrent}" alt="foto ${i + 1}"></img>`;
+let indexCurrent = 2;
+
+
+
+for (let i = 0; i < items.length; i++) {
+
+
+    const img = items[i];
+
+    let classeDaUsare = "";
+
+
+    if (i === indexCurrent) {
+        classeDaUsare = "opacity-100";
+
+    }
+
+    let tagImg = `<img class="${classeDaUsare}" src= "${img}" alt="foto ${i + 1}">`;
 
     containerHero.innerHTML += tagImg;
+
 }
+
+//--------FRECCIA SU----------
+const btnUp = document.querySelector(".fa-chevron-circle-up");
+
+//QUANDO CLICCO SULLA FRECCIA, L'ELEMENTO SUCCESSIVO DIVENTA VISIBILE
+btnUp.addEventListener("click", function () {
+
+    //QUESTA È L'IMMAGINE ATTUALE 
+    let currentImg = document.querySelector(".opacity-100");
+
+    //TOLGO LA CLASSE OPACITY
+    currentImg.classList.remove("opacity-100");
+
+    //QUESTO È L'INDICE MENO 1
+    indexCurrent--;
+
+    //FACCIO UNA SPECIE DI ARRAY CON TUTTE LE IMG
+    const allImg = containerHero.querySelectorAll("img");
+
+
+    if (indexCurrent < 0) {
+        //QUANDO ANDIAMO SOTTO L'INDICE 0 
+        //DOBBIAMO METTERE LA CLASSE ACTIVE ALL'ULTIMO ELEMENTO
+        indexCurrent = items.length - 1;
+    }
+
+    //QUESTA È LA NUOVA IMG ATTUTALE (INDEX DICHIARATO PRIMA MENO 1) 
+    let newCurrentImg = allImg[indexCurrent];
+
+    //ALLA NUOVA IMMAGINE ATTUALE AGGIUNGIAMO LA CLASSE OPACITY
+    newCurrentImg.classList.add("opacity-100");
+
+});
+
+
+
+//--------FRECCIA GIÚ----------
+const btnDown = document.querySelector(".fa-chevron-circle-down");
+
+//QUANDO CLICCO SULLA FRECCIA, L'ELEMENTO SUCCESSIVO DIVENTA VISIBILE
+btnDown.addEventListener("click", function () {
+
+    //QUESTA È L'IMMAGINE ATTUALE 
+    let currentImg = document.querySelector(".opacity-100");
+
+    //TOLGO LA CLASSE OPACITY
+    currentImg.classList.remove("opacity-100");
+
+    //QUESTO È L'INDICE MENO 1
+    indexCurrent++;
+
+    //FACCIO UNA SPECIE DI ARRAY CON TUTTE LE IMG
+    const allImg = containerHero.querySelectorAll("img");
+
+    if (indexCurrent > items.length - 1) {
+        //QUANDO ANDIAMO SOTTO L'INDICE 0 
+        //DOBBIAMO METTERE LA CLASSE ACTIVE ALL'ULTIMO ELEMENTO
+        indexCurrent = 0;
+    }
+
+    //QUESTA È LA NUOVA IMG ATTUTALE (INDEX DICHIARATO PRIMA MENO 1) 
+    let newCurrentImg = allImg[indexCurrent];
+
+    //ALLA NUOVA IMMAGINE ATTUALE AGGIUNGIAMO LA CLASSE OPACITY
+    newCurrentImg.classList.add("opacity-100");
+});
+
+
 
 
 
